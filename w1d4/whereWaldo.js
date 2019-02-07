a9e6891a0630a71a16c8b60d01976c634ddbd3a0
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-07 19:28:35 +0000

    where's Waldo exercise

---
+++ w1d4/whereWaldo.js
@@ -0,0 +1,16 @@
+//create a function that iterates through with forEach
+//have a call back function return the index value of Waldo with msg
+
+function findWaldo(arr, found) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found(i);   
+      }
+    }
+  }
+  
+  function actionWhenFound(arr) {
+    console.log("Found him! :" + arr);
+  }
+  
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
