cef76c397a76e7578de848d5500769ca8049a7eb
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-07 19:56:25 +0000

    whereWaldo imperative

--- w1d4/whereWaldo.js
+++ w1d4/whereWaldo.js
@@ -2,15 +2,15 @@
 //have a call back function return the index value of Waldo with msg
 
 function findWaldo(arr, found) {
-    arr.forEach(function (element) {
-      if (element === "Waldo") {
-        found(arr.indexOf(element));   
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found(i);   
       }
-    })
+    }
   }
   
   function actionWhenFound(arr) {
-    console.log("Found him! :" + arr);
+    console.log("Found him!: " + arr);
   }
   
   findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
